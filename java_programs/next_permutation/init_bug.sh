1abaa144ef16816be8dc9647e07e5b817a2ed0dc
Han325 <yuhan1791@gmail.com>
2024-11-05 22:37:24 +0000

    Added next permutation example

---
+++ java_programs/next_permutation/init_bug.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# patch NEXT_PERMUTATION.java _magpie/NEXT_PERMUTATIONBug.diff
+# srcml NEXT_PERMUTATION.java > _magpie/NEXT_PERMUTATIONBug.java.xml
+mv _magpie/NEXT_PERMUTATIONBug.java.xml NEXT_PERMUTATION.java.xml
